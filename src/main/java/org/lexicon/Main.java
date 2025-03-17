ae39ce3305ab5db9f0303d7ea3f45370704f7af0
JOJO840 <josip840@gmail.com>
2025-03-17 09:44:46 +0100

    Finished Product.

--- src/main/java/org/lexicon/Main.java
+++ src/main/java/org/lexicon/Main.java
@@ -1,7 +1,5 @@
 package org.lexicon;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         //Requirements
--- src/main/java/org/lexicon/MenuDisplay.java
+++ src/main/java/org/lexicon/MenuDisplay.java
@@ -1,7 +1,6 @@
 package org.lexicon;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Scanner;
 
 public class MenuDisplay {
@@ -34,23 +33,40 @@ public class MenuDisplay {
                     System.out.println("you selected 1. Convert SEK to USD");
                     System.out.println("enter in amount you wish to exchange ");
                     sekAmount = scanner.nextDouble();
-
                     double sekToUsdResult = sekAmount;
                     System.out.println("You entered an amount of: " + sekToUsdResult);
                     System.out.println("and your change from SEK to USD is with current rate of: " + CurrencyRates.SEK_TO_USD_RATE + "\n"
                             + "your exchange to USD is: " + CurrencyRates.exchangeRateSekToUsd(sekToUsdResult) + "$");
-
                     break;
 
                 case 2:
-                    System.out.println("you selected 2.");
+                    System.out.println("you selected 2. Convert USD to SEK ");
+                    System.out.println("enter in amount you wish to exchange ");
+                    usdAmount = scanner.nextDouble();
+                    double usdToSekResult = usdAmount;
+                    System.out.println("You entered an amount of: " + usdToSekResult);
+                    System.out.println("and your change from USD to SEK is with current rate of: " + CurrencyRates.USD_TO_SEK_RATE + "\n"
+                            + "your exchange to SEK is: " + CurrencyRates.exchangeRateUsdToSek(usdToSekResult) + "$");
                     break;
+
                 case 3:
-                    System.out.println("you selected 3.");
+                    System.out.println("you selected 3. Convert SEK to Euro");
+                    System.out.println("enter in amount you wish to exchange ");
+                    sekAmount = scanner.nextDouble();
+                    double sekToEuroResult = sekAmount;
+                    System.out.println("You entered an amount of: " + sekToEuroResult);
+                    System.out.println("and your change from SEK to Euro is with current rate of: " + CurrencyRates.SEK_TO_EURO_RATE + "\n"
+                            + "your exchange to Euro is: " + CurrencyRates.exchangeRateSekToEuro(sekToEuroResult) + "$");
                     break;
 
                 case 4:
-                    System.out.println("you selected 4.");
+                    System.out.println("you selected 4. Convert Euro to SEK");
+                    System.out.println("enter in amount you wish to exchange ");
+                    euroAmount = scanner.nextDouble();
+                    double euroToSekResult = euroAmount;
+                    System.out.println("You entered an amount of: " + euroToSekResult);
+                    System.out.println("and your change from Euro to SEK is with current rate of: " + CurrencyRates.EURO_TO_SEK_RATE + "\n"
+                            + "your exchange to SEK is: " + CurrencyRates.exchangeRateEuroToSek(euroToSekResult) + "$");
                     break;
 
                 case 0:
